aafcc874227b183e991d865c0410cfc232646b12
Mitchell Lee <mitchell.lee@yale.edu>
2020-04-18 20:51:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
-# Your Code Here!
+name = "Mitchell Lee"
+age = 22
+favorite_food = "pho"
+
+height_in_inches = 66
+
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
 
